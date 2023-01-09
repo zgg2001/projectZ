37b4e3cf8d5e01957fc7f7d679b08277c5d91fae
zgg2001 <397606244@qq.com>
2023-01-09 23:27:02 +0800

    树莓派 车牌识别逻辑优化

--- raspberry/script/raspberry.py
+++ raspberry/script/raspberry.py
@@ -2,21 +2,26 @@ import RPi.GPIO as GPIO
 import time
 from hyperlpr import *
 import cv2
+import os
 
-# 摄像头超声波
+# 摄像头超声波传感
 Trig_Pin1 = 22
 Echo_Pin1 = 27
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(Trig_Pin1, GPIO.OUT, initial = GPIO.LOW)
 GPIO.setup(Echo_Pin1, GPIO.IN)
 
-# 读取摄像头设备/设置分辨率
-cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+# 车牌识别最短间隔/秒
+detection_interval = 2
+# 读取摄像头设备/设置分辨率/创建tmp目录
+cap = cv2.VideoCapture('/dev/video0') #, cv2.CAP_V4L
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+dirs = './tmp'
+if not os.path.exists(dirs):
+    os.makedirs(dirs)
 
-time.sleep(2)
+time.sleep(1)
 
 def checkdist():
     GPIO.output(Trig_Pin1, GPIO.HIGH)
@@ -31,20 +36,18 @@ def checkdist():
     return (t2 - t1) * 340 * 100 / 2
 
 try:
+    last_time = time.time()
     while True:
+        ret, frame = cap.read()
         distance = checkdist()
-        if distance < 25:
-            time.sleep(1)
-            ret, frame = cap.read()
-            if ret:
-                # 拍照
-                print("ka cha")
-                cv2.imwrite('./tmp/tmp1.jpg', frame) 
+        if distance < 25 and time.time() - last_time > detection_interval:
+            # 拍照
+            cv2.imwrite('./tmp/tmp.jpg', frame) 
             # 分析
-            image = cv2.imread("./tmp/tmp1.jpg")
+            image = cv2.imread('./tmp/tmp.jpg')
             print(HyperLPR_plate_recognition(image))
-            time.sleep(1)
-        time.sleep(1)
+            last_time = time.time()
 except KeyboardInterrupt:
     GPIO.cleanup()
     cap.release()
+    cv2.destroyAllWindows()
