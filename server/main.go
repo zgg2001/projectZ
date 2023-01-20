7171569ac1881072989012f225c283b3e747b3ad
zgg2001 <397606244@qq.com>
2023-01-20 17:23:35 +0800

    服务端 rpc连接加密验证

--- server/main.go
+++ server/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
 	"log"
 	"net"
 
@@ -8,14 +11,34 @@ import (
 	"github.com/zgg2001/projectZ/server/pkg/rpc"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 func main() {
+
+	cert, err := tls.LoadX509KeyPair("./auth/server.pem", "./auth/server.key")
+	if err != nil {
+		log.Fatal("TLS error", err)
+		return
+	}
+	certPool := x509.NewCertPool()
+	ca, err := ioutil.ReadFile("./auth/ca.crt")
+	if err != nil {
+		log.Fatal("NewCertPool error", err)
+		return
+	}
+	certPool.AppendCertsFromPEM(ca)
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    certPool,
+	})
+
 	listen, err := net.Listen("tcp", ":8888")
 	if err != nil {
 		log.Println(err)
 	}
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(grpc.Creds(creds))
 	rpc.RegisterProjectServiceServer(grpcServer, operate.CheckService)
 	err = grpcServer.Serve(listen)
 	if err != nil {
