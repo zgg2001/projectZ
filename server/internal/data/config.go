7cd4bd097e958cd7f4e5a5bd9a77600fc9549aea
zgg2001 <397606244@qq.com>
2023-02-12 21:30:17 +0800

    服务端 sql语句格式调整

--- server/internal/data/config.go
+++ server/internal/data/config.go
@@ -8,7 +8,7 @@ import (
 // mysql
 const (
 	DriverName     = "mysql"
-	DataSourceName = "root:password@/projectZ"
+	DataSourceName = "root:Zhj@7512@/projectZ"
 )
 
 var (
@@ -21,10 +21,29 @@ var (
 const (
 	SqlGetTableNum = "SELECT COUNT(TABLE_NAME) FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'projectZ';"
 
-	SqlCreateParkingTbl = "CREATE TABLE `z_parking` (`id` int unsigned NOT NULL DEFAULT '0', `count` int unsigned NOT NULL DEFAULT '0', `info` varchar(255) NOT NULL DEFAULT '', PRIMARY KEY (`id`));"
-	SqlCreateUserTbl    = "CREATE TABLE `z_user` (`id` int unsigned NOT NULL AUTO_INCREMENT, `username` varchar(255) NOT NULL DEFAULT 'user', `balance` int unsigned NOT NULL DEFAULT '0', `creation_time` bigint unsigned DEFAULT '0', `last_Modified` bigint unsigned DEFAULT '0', PRIMARY KEY (`id`));"
-	SqlCreateLicenseTbl = "CREATE TABLE `z_license` (`id` int unsigned NOT NULL DEFAULT '0', `license` varchar(255) NOT NULL DEFAULT '豫A88888', `checkin_time` bigint unsigned DEFAULT '0', PRIMARY KEY (`id`));"
-	SqlCreateRecordTbl  = "CREATE TABLE `z_record` (`license` varchar(255) NOT NULL DEFAULT '豫A88888', `pid` int unsigned NOT NULL DEFAULT '0', `sid` int unsigned NOT NULL DEFAULT '0', `entry_time` bigint unsigned DEFAULT '0', PRIMARY KEY (`license`));"
+	SqlCreateParkingTbl = "CREATE TABLE `z_parking` (" +
+		"`id` int unsigned NOT NULL DEFAULT '0'," +
+		"`count` int unsigned NOT NULL DEFAULT '0'," +
+		"`info` varchar(255) NOT NULL DEFAULT ''," +
+		"PRIMARY KEY (`id`));"
+	SqlCreateUserTbl = "CREATE TABLE `z_user` (" +
+		"`id` int unsigned NOT NULL AUTO_INCREMENT, " +
+		"`username` varchar(255) NOT NULL DEFAULT 'user'," +
+		"`balance` int unsigned NOT NULL DEFAULT '0', " +
+		"`creation_time` bigint unsigned DEFAULT '0', " +
+		"`last_Modified` bigint unsigned DEFAULT '0', " +
+		"PRIMARY KEY (`id`));"
+	SqlCreateLicenseTbl = "CREATE TABLE `z_license` (" +
+		"`id` int unsigned NOT NULL DEFAULT '0'," +
+		"`license` varchar(255) NOT NULL DEFAULT '豫A88888'," +
+		"`checkin_time` bigint unsigned DEFAULT '0', " +
+		"PRIMARY KEY (`id`));"
+	SqlCreateRecordTbl = "CREATE TABLE `z_record` (" +
+		"`license` varchar(255) NOT NULL DEFAULT '豫A88888'," +
+		"`pid` int unsigned NOT NULL DEFAULT '0'," +
+		"`sid` int unsigned NOT NULL DEFAULT '0'," +
+		"`entry_time` bigint unsigned DEFAULT '0'," +
+		"PRIMARY KEY (`license`));"
 
 	SqlSelectParkingTbl = "SELECT * FROM z_parking;"
 	SqlSelectUserTbl    = "SELECT * FROM z_user;"
