d50179c3675d2b6ad97d5f273e0bbccc60d01c0a
zgg2001 <397606244@qq.com>
2023-03-26 14:43:51 +0800

    windows grpc 调试成功!

--- windows_client/widget.cpp
+++ windows_client/widget.cpp
@@ -1,4 +1,5 @@
 #include "widget.h"
+#include <QDebug>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
@@ -6,13 +7,47 @@ Widget::Widget(QWidget *parent)
     _WIDTH = static_cast<int>(GetSystemMetrics(SM_CXSCREEN)/2);
     _HEIGHT = static_cast<int>(GetSystemMetrics(SM_CYSCREEN)/2);
 
-    setWindowTitle(tr("projectZ"));
+    setWindowTitle(tr("projectZ for windows"));
     setMinimumSize(_WIDTH,_HEIGHT);
     setMaximumSize(_WIDTH,_HEIGHT);
     setStyleSheet("color: gray; background: silver;");//border: 1px solid green;
+
+    std::string address("*.zgg2001.com:8888");
+    auto channel = grpc::CreateChannel(address, grpc::InsecureChannelCredentials());
+    _stub = ProjectService::NewStub(channel);
+
+    ClientContext context;
+    UserLoginRequest request;
+    UserLoginResponse response;
+
+    request.set_username("test");
+    request.set_password("test123456");
+
+    Status status = _stub->UserLogin(&context, request, &response);
+    if(status.ok())
+    {
+        qDebug()<<"success";
+    }
+    else
+    {
+        qDebug()<< status.error_code();
+        qDebug()<< QString::fromStdString(status.error_message());
+    }
 }
 
 Widget::~Widget()
 {
 }
+
+std::string Widget::read_file(std::string file_path)
+{
+    std::ifstream f{file_path.c_str(), std::ios::binary};
+    std::stringstream buffer;
+    buffer << f.rdbuf();
+    f.close();
+    return buffer.str();
+}
+
+
+
 
--- windows_client/widget.h
+++ windows_client/widget.h
@@ -1,8 +1,20 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
-#include <QWidget>
 #include <windows.h>
+
+#include <QWidget>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+
+#include <grpcpp/grpcpp.h>
+#include "pbfile/service.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
 
 class Widget : public QWidget
 {
@@ -13,8 +25,12 @@ public:
     ~Widget();
 
 private:
+    std::string read_file(std::string file_path);
+
+private:
     int _WIDTH;
     int _HEIGHT;
 
+    std::unique_ptr<ProjectService::Stub> _stub;
 };
 #endif // WIDGET_H
